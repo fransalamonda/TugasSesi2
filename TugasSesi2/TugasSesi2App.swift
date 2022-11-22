ef95b259005d022665178fc4f53a4b2e70d31684
franssalamonda <mondlightmoon@gmail.com>
2022-11-23 06:09:46 +0700

    Initial Commit

---
+++ TugasSesi2/TugasSesi2App.swift
@@ -0,0 +1,17 @@
+//
+//  TugasSesi2App.swift
+//  TugasSesi2
+//
+//  Created by Frans Sala Monda on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TugasSesi2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
